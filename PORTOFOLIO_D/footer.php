592aca09fca19ecf829b98821753b22770605cc7
abiek <127909259+abiek@users.noreply.github.com>
2026-07-02 17:13:12 +0700

    Delete PORTOFOLIO_D directory

--- PORTOFOLIO_D/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center py-3">
-    <div class="container">
-        <p>&copy; <?= date('Y') ?> DINA NABILA SARI. All Rights Reserved.</p>
-    </div>
-</footer>
-
-</body>
-</html>
